b31bd52b5813d538fa7f2410332fa36d90866313
uncleboo2610 <86064458+uncleboo2610@users.noreply.github.com>
2023-03-01 10:46:49 +0700

    Add files via upload

---
+++ server/configuration/db.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+	var err error
+	dsn := "host=localhost user=postgres password=Parasol26102001 dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+}
